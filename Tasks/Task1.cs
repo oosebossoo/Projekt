f9c08fd1be7152ceac48e7b0e0dd90c0166b5c37
116926 <Sebastian@DESKTOP-7IFTIR8>
2024-07-19 16:45:13 +0200

    update

--- Tasks/Task1.cs
+++ Tasks/Task1.cs
@@ -1,34 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projekt.Tasks
+﻿namespace Projekt.Tasks
 {
+    
     public static class Task1
     {
         public static void Equation()
         {
-            double a = 1;
-            double b = -2;
-            double c = 1;
+            List<double> xData = new List<double> { -1, 0, 1, 2 };
+            List<double> yData = new List<double> { 1, 0, 1, 6 };
 
-            List<double> xTable = new List<double>();
-            List<double> fTable = new List<double>();
+            double[] coefficients = CalculateQuadraticLeastSquares(xData, yData);
 
-            for (double x = -1.0; x <= 2.0; x += 0.2)
+            double a = coefficients[0];
+            double b = coefficients[1];
+            double c = coefficients[2];
+
+            Console.WriteLine("Wyznaczone współczynniki:");
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"c = {c}");
+
+            Console.WriteLine("\nTablicowanie funkcji F(x) = ax^2 + bx + c:");
+            double step = 0.2;
+            for (double x = -1; x <= 2; x += step)
             {
-                double fx = a * x * x + b * x + c;
-                xTable.Add(x);
-                fTable.Add(fx);
+                double y = a * x * x + b * x + c;
+                Console.WriteLine($"F({x:F1}) = {y:F4}");
+            }
+        }
+
+        static double[] CalculateQuadraticLeastSquares(List<double> xData, List<double> yData)
+        {
+            int n = xData.Count;
+
+            double sumX = xData.Sum();
+            double sumX2 = xData.Sum(x => x * x);
+            double sumX3 = xData.Sum(x => x * x * x);
+            double sumX4 = xData.Sum(x => x * x * x * x);
+            double sumY = yData.Sum();
+            double sumXY = xData.Zip(yData, (x, y) => x * y).Sum();
+            double sumX2Y = xData.Zip(yData, (x, y) => x * x * y).Sum();
+
+            double[,] A = new double[3, 3];
+            double[] B = new double[3];
+
+            A[0, 0] = n;
+            A[0, 1] = sumX;
+            A[0, 2] = sumX2;
+            A[1, 0] = sumX;
+            A[1, 1] = sumX2;
+            A[1, 2] = sumX3;
+            A[2, 0] = sumX2;
+            A[2, 1] = sumX3;
+            A[2, 2] = sumX4;
+
+            B[0] = sumY;
+            B[1] = sumXY;
+            B[2] = sumX2Y;
+
+            double[] coefficients = GaussianElimination(A, B);
+
+            return coefficients;
+        }
+
+        static double[] GaussianElimination(double[,] A, double[] B)
+        {
+            int n = B.Length;
+            double[] X = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int max = i;
+                for (int k = i + 1; k < n; k++)
+                {
+                    if (Math.Abs(A[k, i]) > Math.Abs(A[max, i]))
+                    {
+                        max = k;
+                    }
+                }
+
+                for (int k = i; k < n; k++)
+                {
+                    double tmp = A[max, k];
+                    A[max, k] = A[i, k];
+                    A[i, k] = tmp;
+                }
+                {
+                    double tmp = B[max];
+                    B[max] = B[i];
+                    B[i] = tmp;
+                }
+
+                for (int k = i + 1; k < n; k++)
+                {
+                    double factor = A[k, i] / A[i, i];
+                    B[k] -= factor * B[i];
+                    for (int j = i; j < n; j++)
+                    {
+                        A[k, j] -= factor * A[i, j];
+                    }
+                }
             }
 
-            Console.WriteLine("x\tF(x)");
-            for (int i = 0; i < xTable.Count; i++)
+            for (int i = n - 1; i >= 0; i--)
             {
-                Console.WriteLine($"{xTable[i]:F2}\t{fTable[i]:F6}");
+                double sum = 0;
+                for (int j = i + 1; j < n; j++)
+                {
+                    sum += A[i, j] * X[j];
+                }
+                X[i] = (B[i] - sum) / A[i, i];
             }
+
+            return X;
         }
     }
 }
--- Tasks/Task2.cs
+++ Tasks/Task2.cs
@@ -8,8 +8,8 @@ namespace Projekt.Tasks
 {
     public class Task2
     {
-        static double[] xValues = { -1.0, 0.0, 1.0, 2.0, 3.0 };
-        static double[] fValues = { 1.0, 0.0, 1.0, 4.0, 9.0 };
+        static double[] xValues = { -1.0, 0.0, 1.0, 2.0 };
+        static double[] fValues = { 1.0, 0.0, 1.0, 6.0 };
 
         public static double LagrangeInterpolation(double x)
         {
@@ -57,3 +57,77 @@ namespace Projekt.Tasks
         }
     }
 }
+
+//using System;
+
+//namespace Projekt.Tasks
+//{
+//    public class Task2
+//    {
+//        public static void Equation()
+//        {
+//            // Współrzędne punktów
+//            double[][] punkty = {
+//                new double[] { -1, -1 },
+//                new double[] { 0, 0 },
+//                new double[] { 1, 1 },
+//                new double[] { 2, 4 },
+//                new double[] { 3, 9 }
+//            };
+
+//            // Tworzenie wielomianu interpolacyjnego Lagrange'a
+//            double[] wspolczynnikiLagrange = Lagrange(punkty);
+
+//            // Tworzenie funkcji kwadratowej
+//            double a = 1;
+//            double b = 2;
+//            double c = 1;
+
+//            // Tablica wartości
+//            Console.WriteLine("x | W3(x) | F(x)");
+//            Console.WriteLine("--|-------|------");
+//            for (double x = -1; x <= 3; x += 0.2)
+//            {
+//                // Wartość wielomianu interpolacyjnego Lagrange'a
+//                double w3 = 0;
+//                for (int i = 0; i < wspolczynnikiLagrange.Length; i++)
+//                {
+//                    w3 += wspolczynnikiLagrange[i] * Math.Pow(x, i);
+//                }
+
+//                // Wartość funkcji kwadratowej
+//                double f = a * x * x + b * x + c;
+
+//                // Wyświetlanie wartości
+//                Console.WriteLine($"{x,-3} | {w3,-6:F4} | {f,-4:F2}");
+//            }
+//        }
+
+//        static double[] Lagrange(double[][] punkty)
+//        {
+//            int n = punkty.Length;
+//            double[] wspolczynniki = new double[n];
+
+//            for (int i = 0; i < n; i++)
+//            {
+//                double licznik = 1;
+//                double mianownik = 1;
+
+//                for (int j = 0; j < n; j++)
+//                {
+//                    if (i == j)
+//                    {
+//                        continue;
+//                    }
+
+//                    licznik *= punkty[i][0] - punkty[j][0];
+//                    mianownik *= punkty[i][0] - punkty[j][0];
+//                }
+
+//                wspolczynniki[i] = licznik / mianownik;
+//            }
+
+//            return wspolczynniki;
+//        }
+//    }
+//}
